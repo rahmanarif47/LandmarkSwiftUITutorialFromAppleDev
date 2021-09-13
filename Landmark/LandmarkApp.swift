c63f3333634177f7f5ec1d20b39d672168724660
rahmanarif47 <rahmanarif.9764@gmail.com>
2021-09-13 08:55:36 +0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Arif Rahman Sidik on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
