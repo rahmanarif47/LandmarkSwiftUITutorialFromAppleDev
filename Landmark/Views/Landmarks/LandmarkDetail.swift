6101d85728ccdd4f8b5b4b163289df003eee36a1
rahmanarif47 <rahmanarif.9764@gmail.com>
2021-09-28 20:06:18 +0700

    finish from tutorial, let begin to modify

--- Landmark/Views/Landmarks/LandmarkDetail.swift
+++ Landmark/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData : ModelData
     var landmark: Landmark
+    
+    //Index data by id
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    }
     
     var body: some View {
         ScrollView {
@@ -21,9 +27,12 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -120)
             
             VStack(alignment: .leading){
-                Text(landmark.name)
-                    .font(.title)
-                    .foregroundColor(.primary)
+                HStack{
+                    Text(landmark.name)
+                        .font(.title)
+                        .foregroundColor(.primary)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 HStack {
                     Text(landmark.park)
@@ -50,7 +59,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
     static var previews: some View {
-        LandmarkDetail(landmark: ModelData().landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[1])
+            .environmentObject(modelData)
     }
 }
--- Landmark/Views/Landmarks/LandmarkList.swift
+++ Landmark/Views/Landmarks/LandmarkList.swift
@@ -40,6 +40,7 @@ struct LandmarkList_Previews: PreviewProvider {
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
+                .environmentObject(ModelData())
         }
     }
 }
